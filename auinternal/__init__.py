f9b1e9d1bbf83c4e31a10707910920ef88259243
manoj <manoj.prithvee@gmail.com>
2015-05-15 00:22:28 +0530

    inital commet

--- auinternal/__init__.py
+++ auinternal/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.session import SignedCookieSessionFactory
 
 
 def main(global_config, **settings):
@@ -7,12 +8,15 @@ def main(global_config, **settings):
     It is usually called by the PasteDeploy framework during 
     ``paster serve``.
     """
-
+    my_session_factory = SignedCookieSessionFactory('itsaseekreet')
     config = Configurator(settings=settings)
     config.add_translation_dirs('locale/')
     config.include('pyramid_jinja2')
+    config.set_session_factory(my_session_factory)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('table', '/table')
+    config.add_route('formcheck', '/formcheck')
+    config.add_route('session', '/session')
     config.scan()
     return config.make_wsgi_app()
--- auinternal/views.py
+++ auinternal/views.py
@@ -1,5 +1,6 @@
 from pyramid.view import view_config
 import pymongo
+from pyramid.response import Response
 conn=pymongo.MongoClient()
 db=conn.student
 
@@ -13,3 +14,24 @@ def table(request):
     find=db.student.find_one({'_id':'110113205003'})
     return dict(title='table', table=find['iassmark'],len=len(find['iassmark'][1]))
 
+
+@view_config(route_name='formcheck', renderer='templates/home.jinja2')
+def form(request):
+    maonj=request.POST.items()
+    return dict(title='table', table=maonj ,len=2)
+
+
+
+@view_config(route_name='session')
+def myview(request):
+    session = request.session
+    if 'abc' in session:
+        session['fred'] = 'yes'
+    session['abc'] = '123'
+    if 'fred' in session:
+        manoj=session['abc']
+        session.invalidate()
+        return Response(manoj)
+
+    else:
+        return Response('Fred was not in the session')
